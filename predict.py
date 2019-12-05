b8cea37d27e95e0f1842691ffbc990ffddcf88f0
Bartosz Malcherek <bartoszmalcherek@outlook.com>
2019-12-05 16:55:39 +0100

    EDIT predict.py and add test_images/ to predict

--- predict.py
+++ predict.py
@@ -1,22 +1,31 @@
 import torch
 from torchvision import transforms
+from torchvision.models import resnet50
+
 from PIL import Image
+import os
 
 classes = ['airplane', 'bird', 'building', 'deer', 'dog', 'frog', 'horse', 'ship', 'sportscar', 'truck']
 
-model = torch.load('models/resnet50-medium_dataset-with_reduce_lr_on_plateau-scheduler-100epochs')
+model = resnet50()
+model.fc = torch.nn.Linear(2048, 10, bias=True)
+model.load_state_dict(torch.load('final_state_dict', map_location=torch.device('cpu')))
 model.eval()
 
 transform = transforms.ToTensor()
 
-img = Image.open('horse.jpg')
-img = img.resize((256, 256), Image.ANTIALIAS)
-img = img.convert('RGB')
+for image in os.listdir('test_images'):
+    try:
+        img = Image.open(f'test_images/{image}')
+        img = img.resize((256, 256), Image.ANTIALIAS)
+        img = img.convert('RGB')
 
-img = transform(img)
-img = img.view(1, 3, 256, 256).cuda()
-output = model(img).cpu()
+        img = transform(img)
+        img = img.view(1, 3, 256, 256)
+        output = model(img)
 
-prediction = int(torch.max(output.data, 1)[1].numpy())
+        prediction = int(torch.max(output.data, 1)[1].numpy())
 
-print(classes[prediction])
+        print(f'Image {image}, predicted class: {classes[prediction]}')
+    except:
+        print(f'{image} failed')
